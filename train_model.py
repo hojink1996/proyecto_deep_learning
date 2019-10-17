e2fc6fca08721679d7cffaced91fe166e56e384f
Hojin <hojink1996@gmail.com>
2019-10-17 02:07:17 -0300

    Add code for training

--- train_model.py
+++ train_model.py
@@ -4,7 +4,27 @@ Do the training for the model
 import torch
 
 from lib.RNN import RNN
+from torch import optim, nn
 
+"""
+Parameters
+"""
+# Parameters for the network
+hidden_layer_neurons = 20
+
+# Parameters for training data
+data_per_example = 5
+batch_size = 20
+example_size = 10
+
+# Parameters for learning
+lr = 0.01
+dropout_probability = 0.5
+n_epochs = 100
+
+"""
+Predictions and training
+"""
 # Check if CUDA is available
 is_cuda = torch.cuda.is_available()
 
@@ -18,14 +38,33 @@ else:
 
 
 # Define the RNN
-rnn = RNN(10, 20, dropout_prob=0.5)
+rnn = RNN(example_size, hidden_layer_neurons, dropout_prob=dropout_probability)
 rnn.to(device)
 
 # Generate some test data
-data_per_example = 5
-batch_size = 20
-example_size = 10
 test_data = torch.randn(data_per_example, batch_size, example_size).to(device)
 
 # Do the forward of the data
 results = rnn.forward(test_data)
+
+# Loss function and optimizer
+loss = nn.CrossEntropyLoss()
+optimizer = optim.Adam(rnn.parameters(), lr=lr)
+
+
+training_data = []
+# Train
+for epoch in range(n_epochs):
+    rnn.train()
+
+    for index in training_data:
+        # Make gradients zero
+        optimizer.zero_grad()
+
+        # Forward
+        outputs = rnn.forward(test_data)
+
+        # Value for loss
+        # loss_value = loss(outputs, tags)
+        # loss_value.backward()
+        optimizer.step()
